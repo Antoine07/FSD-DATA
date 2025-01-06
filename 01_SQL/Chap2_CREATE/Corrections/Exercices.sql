1550f3d804585048063a05e1808ec6e0fffcff53
Antoine07 <antoine.lucsko@gmail.com>
2025-01-06 14:25:33 +0100

    create table

--- 01_SQL/Chap2_CREATE/Corrections/Exercices.sql
+++ 01_SQL/Chap2_CREATE/Corrections/Exercices.sql
@@ -20,3 +20,16 @@ ALTER TABLE `companies` DROP COLUMN `num_street`;
 -- ajout de la colonne name
 
 ALTER TABLE `companies` ADD `num_street` SMALLINT UNSIGNED AFTER `city` ;
+
+
+CREATE TABLE `pilots`(
+    `certificate` VARCHAR(6),
+    `company`  CHAR(4),
+    `num_flying` DECIMAL(5,1), -- 5 chiffres 4 chiffres avant virgule et 1 après
+    `name` VARCHAR(20),
+    CONSTRAINT `pk_certificate` PRIMARY KEY (`certificate`),
+    CONSTRAINT `uq_name` UNIQUE(`name`)
+)ENGINE=InnoDB ;
+
+ALTER TABLE `pilots` 
+ADD CONSTRAINT `fk_pilots_company` FOREIGN KEY (`company`) REFERENCES `companies`(`comp`);
